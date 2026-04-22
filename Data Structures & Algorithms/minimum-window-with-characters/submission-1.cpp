3b69ff83ab25d40e601e7036bee2cc15c78c8426
NoVt07 <145884490+NoVt07@users.noreply.github.com>
2026-04-22 16:43:27 +0700

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string minWindow(string s, string t){
+        vector<int> need(128, 0);
+        for(char c : t) need[c]++;
+
+        int left = 0, right = 0;
+        int required = t.length();
+    
+        int minlen = INT_MAX;
+        int minStart = 0;
+        int n = s.length();
+
+        while(right < n){
+            char c = s[right];
+            if(need[c] > 0){
+                required--;
+            }
+            need[c]--;
+            right++;
+            while(required == 0){
+                if(right - left < minlen){
+                    minlen = right - left;
+                    minStart = left;
+                }
+                char leftChar = s[left];
+                need[leftChar]++;
+                if(need[leftChar] > 0){
+                    required++;
+                }
+                left++;
+            }
+        }
+        return minlen == INT_MAX ? "" : s.substr(minStart, minlen);
+    }
+};
