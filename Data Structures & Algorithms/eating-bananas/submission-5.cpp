a2b9d79a3bb52e9e37ae5a0b060e327841ddad7c
NoVt07 <145884490+NoVt07@users.noreply.github.com>
2026-04-23 22:47:24 +0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool check(vector<int> piles, int k, int h){
+        for(int x : piles){
+            int y = (x % k == 0) ? x/k : x/k + 1;
+            h -= y; 
+        }
+        return h >= 0;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        if(h == n){
+            int max_k = 0;
+            for(int x : piles){
+                if(x > max_k) max_k = x;
+            }
+            return max_k;
+        }
+
+        int left = 1, right = 1000000000, mid = 0;
+        while(left <= right){
+            mid = (left + right) / 2;
+            cout << left << " " << right<<"\n";
+            if(check(piles, mid, h)) right = mid - 1;
+            else left = mid + 1;  
+        }
+        return left;
+    }
+};
