47ee19d2d32c48fd76b2c36c2379d023c4bc38f1
NoVt07 <145884490+NoVt07@users.noreply.github.com>
2026-05-22 19:35:22 +0700

    Add: count-good-nodes-in-binary-tree - submission-0

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-0.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int dfs(TreeNode* root, int m) {
+        if(!root) return 0;
+
+        int good = 0;
+        if(root->val >= m) {
+            good = 1;
+            m = root->val;
+        }
+        return good + dfs(root->left, m) + dfs(root->right, m);
+    }
+
+    int goodNodes(TreeNode* root) {
+        return dfs(root, root->val);
+    }
+};
